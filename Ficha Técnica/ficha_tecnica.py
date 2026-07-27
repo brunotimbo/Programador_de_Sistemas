36466e3b9ebd9ce57d0e41fb41345eab004dc76b
BrunoTimbo <brunoluis00@gmail.com>
2026-07-27 11:26:54 -0300

    inicio banco de dados

--- Ficha Técnica/ficha_tecnica.py
+++ Ficha Técnica/ficha_tecnica.py
@@ -4,16 +4,17 @@ conexao = sqlite3.connect('ficha_tecnica.db')
 
 cursor = conexao.cursor()
 
-cursor.execute('''
-CREATE TABLE Ficha_Tecnica(
-ID INTEGER PRIMARY KEY,
-Ingrediente TEXT NOT NULL,
-Quantidade_Comprada INTEGER,
-Preco_Comprado REAL,
-Quantidade_Usada INTEGER,
-Unidade TEXT,
-Preco_Gasto REAL
-)
-''')
+# cursor.execute('''
+# CREATE TABLE Ficha_Tecnica(
+# ID INTEGER PRIMARY KEY,
+# Ingrediente TEXT NOT NULL,
+# Quantidade_Comprada INTEGER,
+# Preco_Comprado REAL,
+# Quantidade_Usada INTEGER,
+# Unidade TEXT,
+# Preco_Gasto REAL
+# )
+# ''')
 
-conexao.commit()
+# conexao.commit()
+
